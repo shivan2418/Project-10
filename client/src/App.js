8ad6e3dc1083c50fe7a7287082507cf9e898f029
Emil Elgaard <shivan@shivan.dk>
2020-06-18 16:57:20 -0400

    remove and rename some components

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,28 @@
 import React from 'react';
-import Header from './Components/Header'
-import withContext from './AuthContext'
 import {
-  BrowserRouter,
+  BrowserRouter as Router,
   Route,
   Switch,
 } from 'react-router-dom';
-import Courses from './Components/Courses';
-import CourseDetail from "./Components/CourseDetail";
-import CreateCourse from "./Components/CreateCourse";
-import UpDateCourse from "./Components/UpdateCourse";
-import SignIn from './Components/UserSignIn';
-import SignUp from './Components/UserSignUp';
-import UserSignOut from './Components/UserSignOut';
-import Authenticated from './Components/Authenticated';
+
+import Header from './components/Header';
+import Courses from './components/Courses';
+import CourseDetail from "./components/CourseDetail";
+import CreateCourse from "./components/CreateCourse";
+import UserSignIn from './components/UserSignIn';
+import UserSignUp from './components/UserSignUp';
+import UserSignOut from './components/UserSignOut';
+import Authenticated from './components/Authenticated';
+
+import withContext from './AuthContext'
 import PrivateRoute from './PrivateRoute';
 
 const HeaderWithAuth = withContext(Header);
 const CoursesWithAuth = withContext(Courses);
 const CourseDetailWithAuth = withContext(CourseDetail);
 const CreateCourseWithAuth = withContext(CreateCourse);
-const UpdateCourseWithAuth = withContext(UpDateCourse);
-const SignInWithAuth = withContext(SignIn);
-const SignUpWithAuth = withContext(SignUp);
+const SignInWithAuth = withContext(UserSignIn);
+const SignUpWithAuth = withContext(UserSignUp);
 const SignOutWithAuth = withContext(UserSignOut);
 const AuthenticatedWithAuth = withContext(Authenticated);
 
@@ -30,21 +30,21 @@ const AuthenticatedWithAuth = withContext(Authenticated);
 function App() {
 
   return (
-    <>
-      <BrowserRouter>
+    <Router>
+      <div>
         <HeaderWithAuth/>
+
           <Switch>
             <Route exact path ='/' component={CoursesWithAuth} />
             <PrivateRoute exact path = '/courses/create' component={CreateCourseWithAuth}/>
-            <PrivateRoute exact path = '/courses/:id/update' component = {UpdateCourseWithAuth}/>
             <Route exact path = '/courses/:id' component={CourseDetailWithAuth}/>
             <Route exact path = '/signin' component={SignInWithAuth}/>
             <Route exact path = '/signup' component={SignUpWithAuth}/>
-            <PrivateRoute exact path = '/authenticated' component={AuthenticatedWithAuth}/>
             <Route exact path = '/signout' component={SignOutWithAuth}/>
+            <PrivateRoute exact path = '/authenticated' component={AuthenticatedWithAuth}/>
           </Switch>
-      </BrowserRouter>
-    </>
+    </div>
+      </Router>
   );
 }
 
