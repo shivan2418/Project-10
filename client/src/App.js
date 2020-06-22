80c9f7b216b999440ea0a133dea6b7a347178b46
Emil Elgaard <shivan@shivan.dk>
2020-06-21 21:15:49 -0400

    Finishing touches

--- client/src/App.js
+++ client/src/App.js
@@ -9,19 +9,19 @@ import Header from './components/Header';
 import Courses from './components/Courses';
 import CourseDetail from "./components/CourseDetail";
 import CreateCourse from "./components/CreateCourse";
+import UpdateCourse from './components/UpdateCourse';
 import UserSignIn from './components/UserSignIn';
 import UserSignUp from './components/UserSignUp';
 import UserSignOut from './components/UserSignOut';
 import Authenticated from './components/Authenticated';
-
+import Error from './components/Error';
 import withContext from './AuthContext'
 import PrivateRoute from './PrivateRoute';
-
-import Test from './components/Test';
 
 const HeaderWithAuth = withContext(Header);
 const CoursesWithAuth = withContext(Courses);
 const CourseDetailWithAuth = withContext(CourseDetail);
+const UpdateCourseWithAuth = withContext(UpdateCourse);
 const CreateCourseWithAuth = withContext(CreateCourse);
 
 const SignInWithAuth = withContext(UserSignIn);
@@ -29,7 +29,6 @@ const SignUpWithAuth = withContext(UserSignUp);
 const SignOutWithAuth = withContext(UserSignOut);
 const AuthenticatedWithAuth = withContext(Authenticated);
 
-const TestWithAuth = withContext(Test);
 
 function App() {
 
@@ -38,13 +37,15 @@ function App() {
       <div>
         <HeaderWithAuth/>
 
-          <Switch>            <Route exact path ='/' component={CoursesWithAuth} />
+          <Switch>            
+            <Route exact path ='/' component={CoursesWithAuth} />
             <PrivateRoute exact path = '/courses/create' component={CreateCourseWithAuth}/>
             <Route exact path = '/courses/:id' component={CourseDetailWithAuth}/>
             <Route exact path = '/signin' component={SignInWithAuth}/>
+            <PrivateRoute exact path = '/courses/:id/update' component={UpdateCourseWithAuth}/>
             <Route exact path = '/signup' component={SignUpWithAuth}/>
+            <Route exact path ='/error' component={Error}/>
             <Route exact path = '/signout' component={SignOutWithAuth}/>
-            <Route exact path= '/test' component={TestWithAuth}/>
             <PrivateRoute exact path = '/authenticated' component={AuthenticatedWithAuth}/>
           </Switch>
     </div>
